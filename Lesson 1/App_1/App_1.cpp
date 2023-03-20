1ec534103751bd42791c0146cb22b287dee63e4b
Kapitonov Daniil <62303616+daniilkapitonov@users.noreply.github.com>
2023-03-20 20:46:22 +0900

    Lesson 1

---
+++ Lesson 1/App_1/App_1.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+#include <stdio.h>
+#include "App_1.h"
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   /* int j = 1; ТЕОРИЯ + часть первого задания
+    int q = 0;
+    for (int i = 1; i < 10; i++) {
+        cout << i << endl;
+        j *= i;
+    }
+    cout << j << endl;
+
+    printf_s("%d - %d\n", j,q);
+
+    printf_s("Hello\n");
+    char s[50];
+    scanf_s("%s",s, 10);
+    printf_s("%s", s);*/
+
+
+    setlocale(LC_ALL, "Russian");
+    int num = 0;
+    printf_s("Введите число - ");
+    scanf_s("%i", &num);
+    printf_s("Ваше число - %d\n", num);
+    for (int i = 1; i < 10; i++) {
+        printf_s("Умножение на %d = %d\n", i, i*num);
+    }
+    
+
+
+
+}
---
+++ Lesson 1/WindowFrame/MyForm.h
@@ -0,0 +1,106 @@
+#pragma once
+
+namespace WindowFrame {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	protected:
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(525, 524);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(150, 29);
+			this->checkBox1->TabIndex = 0;
+			this->checkBox1->Text = L"checkBox1";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(623, 377);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(229, 100);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(1127, 587);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 31);
+			this->textBox1->TabIndex = 2;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::RosyBrown;
+			this->ClientSize = System::Drawing::Size(1748, 831);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->checkBox1);
+			this->Name = L"MyForm";
+			this->Text = L"Моя первая форма";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
